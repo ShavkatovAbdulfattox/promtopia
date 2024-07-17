eb7fbde9eec0de568fb1af5eb94f18834e266078
Avdulfattox <vwhoami@yahoo.com>
2024-07-17 19:28:25 +0500

    Posts are successfully fetched, get funtionality is working

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+"use client";
+
+import { useEffect, useState } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data?.map((post) => {
+        return <PromptCard key={post._id} post={post} />;
+      })}
+    </div>
+  );
+};
 
 function Feed() {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState();
+  const [posts, setPosts] = useState([]);
+
+  const handleSearchText = () => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch("/api/prompt");
+      const data = await res.json();
+      setPosts(data);
+    };
+
+    fetchPosts();
+  }, []);
+  return (
+    <section className="feed">
+      <div className="w-full relative flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or username"
+          value={searchText}
+          onChange={handleSearchText}
+          className="search_input peer"
+        />
+      </div>
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
 }
 
 export default Feed;
